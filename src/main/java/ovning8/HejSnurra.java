e7cd8261745bd0c0bf3dfba4f66fcfe371e52fb6
EmeHag <emehag-2@student.ltu.se>
2023-04-22 19:37:04 +0200

    hepp

---
+++ src/main/java/ovning8/HejSnurra.java
@@ -0,0 +1,13 @@
+package ovning8;
+
+public class HejSnurra {
+    public static void main(String[] args) {
+        // Initialize a counter variable to 1
+        int i = 1;
+        // Do-while loop to print the number and "Hej!" on the same line
+        do {
+            System.out.println("0" + i + " Hej!");
+            i++; // Increment the counter variable by 1
+        } while (i <= 5); // Loop as long as the counter variable is less than or equal to 5
+    }
+}
